8689fcacfe0c4dfa8251eda759b127ab4362e2cb
Sohelsh <sohelshaikhadam@gmail.com>
2023-11-24 21:20:47 +0530

    Practice

---
+++ 01_SizeOf/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _01_SizeOf
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Size of = {0}",sizeof(short));
+            Console.ReadKey();
+        }
+    }
+}

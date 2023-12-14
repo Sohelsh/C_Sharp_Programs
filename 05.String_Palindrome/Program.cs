4de9161ae63bc929cf74c0b463a5d60140d994bd
Sohelsh <sohelshaikhadam@gmail.com>
2023-12-14 22:23:14 +0530

    Practice

---
+++ 05.String_Palindrome/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _05.String_Palindrome
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string Str, Revs = "";
+
+            Console.WriteLine("Emter String ");
+            Str = Console.ReadLine();
+
+            for(int i = Str.Length - 1; i >= 0;i--)
+            {
+                Revs += Str[i].ToString();
+  
+            }
+
+            if (Str == Revs )
+            {
+                Console.WriteLine("String Is Palindrome = {0}",Str);
+            }
+            else
+            {
+                Console.WriteLine("String Not Palindrome = {0}", Str);
+            }
+
+            Console.ReadLine();
+           
+        }
+    }
+}

361d59f07a938815a2f58145803d7bf0c3fce3b1
Sohelsh <sohelshaikhadam@gmail.com>
2023-12-19 22:35:09 +0530

    Practice

---
+++ 06.Fibonaci_Series/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _06.Fibonaci_Series
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int No1 = 1, No2 = 0, No3 = 0, Cnt = 0;
+
+            Console.WriteLine("Enter Count = ");
+            Cnt = Convert.ToInt32(Console.ReadLine());
+
+            while (Cnt != 0)
+            { 
+                Console.Write(" {0}", No3);
+                No3 = No1 + No2;
+                No1 = No2;
+                No2 = No3;
+                Cnt--;
+            }
+            Console.ReadLine();
+          }
+    }
+}

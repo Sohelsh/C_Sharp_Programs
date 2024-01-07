39502eb156c9c940fd335163f3de4747687a7db5
Sohelsh <sohelshaikhadam@gmail.com>
2024-01-07 23:57:40 +0530

    Practice

---
+++ 08.Factorial_Of_Number/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _08.Factorial_Of_Number
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Fact = 1, No;
+
+            Console.Write("\n Enter Number = ");
+            No = Convert.ToInt32(Console.ReadLine());
+
+            while(No !=  0)
+            {
+                Fact = Fact * No;
+                No--;
+            }
+
+            Console.Write("Factorial Of Number =  {0}",Fact);
+
+            Console.ReadKey();
+        }
+    }
+}
